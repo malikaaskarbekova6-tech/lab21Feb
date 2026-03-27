110f5b9a3f414f4d6dfc2cc7252e8b4b6e859657
malikaaskarbekova6-tech <malikaaskarbekova6@gmail.com>
2026-03-27 23:04:54 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,48 +1,54 @@
 <!DOCTYPE html>
-<html>
+<html lang="ru">
 <head>
     <meta charset="UTF-8">
-    <title>таблица</title>
+    <title></title>
+    <style>
+        table {
+            border-collapse: collapse;
+            border-top: 6px solid gold;
+            border-left: 6px solid gold;
+        }
+
+        td {
+            width: 40px;
+            height: 40px;
+            border: 1px solid black;
+        }
+
+        .black {
+            background-color: black;
+        }
+
+        .white {
+            background-color: white;
+        }
+    </style>
 </head>
 <body>
 
 <form method="post">
-    Введите число: 
-    <input type="number" name="size" min="1" required>
-    <button type="submit">Создать</button>
+    <label>Введите размер доски:</label>
+    <input type="number" name="size">
+    <button type="submit">Показать</button>
 </form>
 
 <?php
 if (isset($_POST['size'])) {
-    $n = (int)$_POST['size'];
+    $n = intval($_POST['size']);
 
-    if ($n > 0) {
-        echo "<table style='border-collapse: collapse;'>";
+    echo "<table border='1'>";
 
-        for ($i = 0; $i < $n; $i++) {
-            echo "<tr>";
-            for ($j = 0; $j < $n; $j++) {
-
-                $color = (($i + $j) % 2 == 0) ? "white" : "black";
-
-                // Добавляем жёлтые линии только внутри таблицы
-                $style = "width:40px; height:40px; background-color:$color;";
-
-                if ($i == 0) {
-                    $style .= " border-top: 5px solid yellow;";
-                }
-
-                if ($j == 0) {
-                    $style .= " border-left: 5px solid yellow;";
-                }
-
-                echo "<td style='$style'></td>";
-            }
-            echo "</tr>";
+    for ($i = 0; $i < $n; $i++) {
+        echo "<tr>";
+        for ($j = 0; $j < $n; $j++) {
+            $color = (($i + $j) % 2 == 0) ? "white" : "black";
+            echo "<td class='$color'></td>";
         }
-
-        echo "</table>";
+        echo "</tr>";
     }
+
+    echo "</table>";
 }
 ?>
 
